e5bc632e4f986bfd417d0815838e4954ab4e5e5d
Crista Cooper <aine3d@gmail.com>
2017-10-20 16:14:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0
 
 
 
@@ -7,6 +8,7 @@ function init() {
 
   let index = 0
   // Write your JavaScript code inside the init() function
+<<<<<<< HEAD
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which)
 
@@ -25,4 +27,23 @@ function init() {
 
     }
 
+=======
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which || e.detail || e.location)
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert('KONAMI WIN! You rock!')
+
+         index = 0;
+      }
+    } else {
+      index = 0
+    }
+
+
+  }
+>>>>>>> a94016fc4d8a95836f3ffbd890aa83b6e1b71044
 }
